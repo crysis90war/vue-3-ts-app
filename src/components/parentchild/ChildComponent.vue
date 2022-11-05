ae06f7afd5bcb0dc94676d8f619250d63bfdb8f0
Samad Hatsijev <s.hatsijev@hotmail.com>
2022-11-05 23:13:39 +0100

    fix: build errors

--- src/components/parentchild/ChildComponent.vue
+++ src/components/parentchild/ChildComponent.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 defineProps(["modelValue"]);
 defineEmits(["update:modelValue"]);
+function addEventListener(event: { target: HTMLInputElement }) {
+  const target = event.target as HTMLInputElement;
+  return target.value;
+}
 </script>
 <template>
   <input
     :value="modelValue"
-    @input="$emit('update:modelValue', $event.target.value)"
+    @input="$emit('update:modelValue', addEventListener)"
   />
 </template>
 <style></style>
--- src/composables/modal.ts
+++ src/composables/modal.ts
@@ -1,13 +1,20 @@
 import SinginForm from "@/components/udemy/SinginForm.vue";
 import SingupForm from "@/components/udemy/SingupForm.vue";
+import * as bootstrap from "bootstrap";
 import { ref, shallowRef } from "vue";
 
 const show = ref<boolean>(false);
 const component = shallowRef();
 const closeModal = (id: string): void => {
   const myModalEl = document.getElementById(id);
+  if (!myModalEl) {
+    throw Error("modal couldn't be found");
+  }
   // eslint-disable-next-line no-undef
   const modal = bootstrap.Modal.getInstance(myModalEl);
+  if (!modal) {
+    throw Error("modal couldn't be found.");
+  }
   modal.hide();
 };
 
